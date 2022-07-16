9f9c7c7ea8742090393a626a2239e9aa9d202356
Zero-Dex <32199855+Zero-Dex0@users.noreply.github.com>
2022-07-17 00:05:11 +0600

    Add files via upload

---
+++ contracts/KuswapFactory.sol
@@ -0,0 +1,61 @@
+pragma solidity =0.5.16;
+
+import './interfaces/IKuswapFactory.sol';
+import './KuswapPair.sol';
+
+contract KuswapFactory is IKuswapFactory {
+    address public feeTo;
+    address public feeToSetter;
+    bytes32 public INIT_CODE_HASH = keccak256(abi.encodePacked(type(KuswapPair).creationCode));
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    constructor(address _feeToSetter) public {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, 'Kuswap: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'Kuswap: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'Kuswap: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(KuswapPair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        IKuswapPair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, 'Kuswap: FORBIDDEN');
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, 'Kuswap: FORBIDDEN');
+        feeToSetter = _feeToSetter;
+    }
+
+    function setDevFee(address _pair, uint8 _devFee) external {
+        require(msg.sender == feeToSetter, 'Kuswap: FORBIDDEN');
+        require(_devFee > 0, 'Kuswap: FORBIDDEN_FEE');
+        KuswapPair(_pair).setDevFee(_devFee);
+    }
+    
+    function setSwapFee(address _pair, uint32 _swapFee) external {
+        require(msg.sender == feeToSetter, 'Kuswap: FORBIDDEN');
+        KuswapPair(_pair).setSwapFee(_swapFee);
+    }
+}
